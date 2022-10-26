392981a829fe28783c311c57cbd8a4f1fda2577e
gusdn5387 <gusdn5387@naver.com>
2022-10-26 10:39:28 +0900

    Initial Commit

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by 박현우 on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
